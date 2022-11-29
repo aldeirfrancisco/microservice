63ff9744007343d4302d5b3a6c3854d16e54c945
aldeir <dide123francisco@gmail.com>
2022-11-29 20:09:32 -0300

    iniciando o progeto

---
+++ src/main/java/br/com/aldeir/controller/SaudacaoController.java
@@ -0,0 +1,26 @@
+package br.com.aldeir.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.aldeir.model.Saudacao;
+
+@RestController
+public class SaudacaoController {
+  
+	 private static final String template = "hello, %s!";
+	 private static final AtomicLong  counter = new AtomicLong();
+	 
+	
+	 @RequestMapping("/saudacao")
+	 public Saudacao greeting(@RequestParam(value = "nome", defaultValue = "World") String nome) {
+		   
+		   
+		 
+		   return new Saudacao(counter.incrementAndGet(),
+             String.format(template,nome));
+	 }
+}
