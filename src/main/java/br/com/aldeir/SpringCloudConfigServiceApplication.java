c76d8bd7d1c7354d7bbe690b0ee276c3edb422be
aldeir <dide123francisco@gmail.com>
2022-11-30 22:19:49 -0300

    testando o spring cloud server local

--- src/main/java/br/com/aldeir/SpringCloudConfigServiceApplication.java
+++ src/main/java/br/com/aldeir/SpringCloudConfigServiceApplication.java
@@ -2,8 +2,11 @@ package br.com.aldeir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+//@EnableConfigServer usando para incorporar um servidor,
+//agora esse aplicativo é um servidor de aplicação
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigServiceApplication {
 
 	public static void main(String[] args) {
