1c2b452f136446308549728ec9a0927cf1bd5696
aldeir <dide123francisco@gmail.com>
2022-12-03 20:26:59 -0300

    recuperando a porta da instância em execução

--- src/main/java/br/com/aldeir/controller/CambioController.java
+++ src/main/java/br/com/aldeir/controller/CambioController.java
@@ -2,6 +2,8 @@ package br.com.aldeir.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +13,10 @@ import br.com.aldeir.model.Cambio;
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
-  
+	
+	@Autowired
+   private Environment environment; // obtenha o valor da propriedade que esteja no properties 
+   
 	//http://localhost:8000/cambio-service/5/USD/BRL
 	@RequestMapping(value = "/{amount}/{from}/{to}" )
 	public Cambio getCambio(
@@ -19,6 +24,9 @@ public class CambioController {
 			@PathVariable("from") String from,
 			@PathVariable("to") String to) {
 		
-		return new Cambio(1L,from, to, BigDecimal.ONE, BigDecimal.ONE, "port: 8000");
+	 var port =	environment.getProperty("local.server.port");//asse varias informações da aplicação
+		
+		
+		return new Cambio(1L,from, to, BigDecimal.ONE, BigDecimal.ONE, port);
 	}
 }
