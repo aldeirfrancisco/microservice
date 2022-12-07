1e1feff0a647ecb2fb90557f499bdb22d84c5fc5
aldeir <dide123francisco@gmail.com>
2022-12-07 20:06:55 -0300

    add feign no pom e pasamos a usar o feign de forma não escalavel para consumir o cambio service

--- src/main/java/br/com/aldeir/BookServiceApplication.java
+++ src/main/java/br/com/aldeir/BookServiceApplication.java
@@ -2,8 +2,10 @@ package br.com.aldeir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
+//@EnableFeignClients habilita o feign no projeto
 @SpringBootApplication
+@EnableFeignClients
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/aldeir/controller/BookController.java
+++ src/main/java/br/com/aldeir/controller/BookController.java
@@ -1,6 +1,5 @@
 package br.com.aldeir.controller;
 
-import java.util.Date;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import br.com.aldeir.model.Book;
+import br.com.aldeir.proxy.CambioProxy;
 import br.com.aldeir.repository.BookRepository;
 import br.com.aldeir.response.Cambio;
 
@@ -24,6 +24,9 @@ public class BookController {
 	
 	@Autowired
 	private BookRepository repository;
+	
+	@Autowired
+	private CambioProxy proxy;
 	
 	//http://localhost:8100/book-service/1/BRL
 	
@@ -40,14 +43,35 @@ public class BookController {
 		params.put("to", currency);
 		var port =	environment.getProperty("local.server.port");
 		
-		var response = new RestTemplate()
-		.getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}", Cambio.class, params);
+		
+		var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
 		 
-		var cambio = response.getBody();
-		 
-		 book.setEnvironment(port);
+		 book.setEnvironment(port + " FEIGN");
 		 book.setPrice(cambio.getConvertedValue());
 		 
 		return book;
 	}
+/*	@GetMapping("/{id}/{currency}")
+	private Book findBook( @PathVariable("id") Long id,
+			@PathVariable("currency") String currency) {
+		HashMap<String, String> params = new HashMap();
+		
+		var book = repository.findById(id).get();
+		if(book == null ) throw new RuntimeException("Book not found");
+		
+		params.put("amount", book.getPrice().toString());
+		params.put("from", "USD");
+		params.put("to", currency);
+		var port =	environment.getProperty("local.server.port");
+		
+		var response = new RestTemplate()
+				.getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}", Cambio.class, params);
+		
+		var cambio = response.getBody();
+		
+		book.setEnvironment(port);
+		book.setPrice(cambio.getConvertedValue());
+		
+		return book;
+	}*/
 }
