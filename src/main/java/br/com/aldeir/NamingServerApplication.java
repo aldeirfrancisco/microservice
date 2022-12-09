2b69246441843fb4de48548ce21c2ed73d378da9
aldeir <dide123francisco@gmail.com>
2022-12-08 23:04:47 -0300

    inicializando o naming-server

---
+++ src/main/java/br/com/aldeir/NamingServerApplication.java
@@ -0,0 +1,14 @@
+package br.com.aldeir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class NamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingServerApplication.class, args);
+	}
+
+}
