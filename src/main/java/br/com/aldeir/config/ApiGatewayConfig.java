0028cc1516491c51d30f92f3704bff2a393628a3
aldeir <dide123francisco@gmail.com>
2022-12-09 23:32:18 -0300

    Rotas com RouteLocator e Spring Cloud Gateway

--- src/main/java/br/com/aldeir/config/ApiGatewayConfig.java
+++ src/main/java/br/com/aldeir/config/ApiGatewayConfig.java
@@ -1,11 +1,6 @@
 package br.com.aldeir.config;
 
-import java.util.function.Function;
-
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,13 +11,16 @@ public class ApiGatewayConfig {
 	@Bean
 	public RouteLocator gatewayRouter( RouteLocatorBuilder builder) {
 		
-		Function<PredicateSpec, Buildable<Route>> funtion=
-				p -> p.path("/get")
+		return builder.routes().
+				route(p -> p.path("/get")
 				.filters(f -> f.addRequestHeader("hello", "world")
 				               .addRequestParameter("hello", "world"))
-				.uri("http://httpbin.org:80");
-		return builder.routes().
-				route(funtion).build();
+				.uri("http://httpbin.org:80"))
+				.route( p -> p.path("/cambio-service/**")
+						.uri("lb://cambio-service"))
+				.route( p -> p.path("/book-service/**")
+						.uri("lb://book-service"))
+				.build();
 	}
 
 }
